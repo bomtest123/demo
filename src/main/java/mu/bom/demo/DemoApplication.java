5a92cccdd41a393f3e4c7d47ce55dcb016518549
bom123 <bomjenkins.test@gmail.com>
2023-01-11 07:02:52 +0000

    resolve java version on pom.xml

--- src/main/java/mu/bom/demo/DemoApplication.java
+++ src/main/java/mu/bom/demo/DemoApplication.java
@@ -1,19 +1,19 @@
 package mu.bom.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Value("${spring.sample.property}")
+	static String valueProperty;
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(DemoApplication.class, args);
-      	
-		@Value("${spring.sample.property}")
-		String valueProperty;
-		System.out.println("Test Demo", valueProperty);
-		
+		System.out.print("Test Demo" + valueProperty);
 	}
 
 }
